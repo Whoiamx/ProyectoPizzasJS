7c0a8042b64590d0d55bacacaaf4fdc04b7268f9
Whoiamx <gaastontimchuk@gmail.com>
2024-03-29 14:59:56 -0300

    Inicio de proyecto EJ 3

---
+++ index.js
@@ -0,0 +1,62 @@
+const d = document;
+const btnPizzas = d.querySelector(".btn-pizzas");
+const inputPizzas = d.querySelector(".input-pizzas");
+const containerPizzas = d.querySelector(".container-pizzas");
+
+const pizzas = [
+  {
+    id: 1,
+    nombre: "pizza de Muzzarella",
+    precio: 500,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+    imagen: "./img/muzzarella.png",
+  },
+
+  {
+    id: 2,
+    nombre: "pizza de Cebolla",
+    precio: 1500,
+    ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+    imagen: "./img/cebolla.png",
+  },
+
+  {
+    id: 3,
+    nombre: "pizza 4 Quesos",
+    precio: 1380,
+    ingredientes: [
+      "Muzzarella",
+      "Tomate",
+      "Queso Azul",
+      "Parmesano",
+      "Roquefort",
+    ],
+    imagen: "./img/4quesos.png",
+  },
+
+  {
+    id: 4,
+    nombre: "pizza Especial",
+    precio: 1000,
+    ingredientes: ["Muzzarella", "Tomate", "Rucula", "Jamón"],
+    imagen: "./img/especial.png",
+  },
+
+  {
+    id: 5,
+    nombre: "pizza con Anana",
+    precio: 600,
+    ingredientes: ["Muzzarella", "Tomate", "Anana"],
+    imagen: "./img/anana.png",
+  },
+];
+const recorridoArrayIds = pizzas.map((pizza) => {
+  let objectNecesario = {
+    id: pizza.id,
+    nombre: pizza.nombre,
+    imagen: pizza.imagen,
+  };
+  return objectNecesario;
+});
+
+console.log(recorridoArrayIds);
